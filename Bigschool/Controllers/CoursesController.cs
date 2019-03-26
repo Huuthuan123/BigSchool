9070ecfd2fc2eafeb28fbf49ff01ef1799ade055
Administrator <Administrator@PC016>
2019-03-26 10:58:18 +0700

    dfghj

---
+++ Bigschool/Controllers/CoursesController.cs
@@ -0,0 +1,18 @@
+﻿using Bigschool.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bigschool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Bigschool/ViewModels/CourseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Bigschool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bigschool.ViewModels
+{
+    public class CourseViewModel
+    {
+        public string Place { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        
+    }
+}
