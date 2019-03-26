33517e761c33c03b841cef2efa49b250185811a7
Administrator <Administrator@PC016>
2019-03-26 11:15:12 +0700

    CourseViewModel

--- Bigschool/ViewModels/CourseViewModel.cs
+++ Bigschool/ViewModels/CourseViewModel.cs
@@ -13,6 +13,9 @@ namespace Bigschool.ViewModels
         public string Time { get; set; }
         public byte Category { get; set; }
         public IEnumerable<Category> Categories { get; set; }
-        
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
     }
 }
